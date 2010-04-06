a86615c2ae9301d33737314c660c597bae710560
Ryan Shaw <ryanshaw@ischool.berkeley.edu>
2010-04-06 16:14:32 -0700

    Fixed bug in text truncation.

--- editorsnotes/main/models.py
+++ editorsnotes/main/models.py
@@ -19,10 +19,18 @@ def xhtml_to_text(xhtml):
     return etree.tostring(textify(xhtml), method='text', encoding=unicode).strip()
 
 def truncate(text, length=100):
+    u"""
+    >>> truncate(u'xxxxxxxxxx', 4)
+    u'xx... xx'
+    """
     if len(text) <= length:
         return text
     l = text[:(length/2)].rsplit(' ', 1)[0]
-    r = text[-(length/2):].split(' ', 1)[1]
+    r = text[-(length/2):].split(' ', 1)
+    if len(r) == 1:
+        r = r[0]
+    else:
+        r = r[1]
     return l + u'... ' + r
 
 class XHTMLWidget(forms.Textarea):
