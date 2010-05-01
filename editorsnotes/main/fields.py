f10809bf8528b7b79e261ed5f59b8794ae8ee0b4
Ryan Shaw <ryanshaw@ischool.berkeley.edu>
2010-04-30 20:30:31 -0700

    Moved XHTML field into separate file.

---
+++ editorsnotes/main/fields.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.db import models
+from lxml import etree, html
+from lxml.html.clean import Cleaner
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
+from django.utils.encoding import force_unicode
+
+cleaner = Cleaner(style=True)
+
+class XHTMLField(models.Field):
+    description = 'A parsed XHTML fragment'
+    __metaclass__ = models.SubfieldBase
+    def __init__(self, *args, **kwargs):
+        super(XHTMLField, self).__init__(*args, **kwargs)
+    def db_type(self):
+        return 'xml'
+    def to_python(self, value):
+        if isinstance(value, html.HtmlElement):
+            return value
+        if not (isinstance(value, str) or isinstance(value, unicode)):
+            raise TypeError('%s cannot be parsed to XHTML' % type(value))
+        if len(value) == 0:
+            return None
+        #value = re.sub(r'(&#13;\n)+', ' ', value)
+        fragment = None
+        try:
+            fragment = html.fragment_fromstring(value)
+        except etree.ParserError:
+            fragment = html.fragment_fromstring(value, create_parent='div')
+        return cleaner.clean_html(fragment)
+    def get_db_prep_value(self, value):
+        return etree.tostring(value)
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
+    def formfield(self, **kwargs):
+        defaults = {'widget': XHTMLWidget}
+        defaults.update(kwargs)
+        return super(XHTMLField, self).formfield(**defaults)
+
+class XHTMLWidget(forms.Textarea):
+    def _format_value(self, value):
+        if value is None:
+            return ''
+        if isinstance(value, html.HtmlElement):
+            return etree.tostring(value)
+        if isinstance(value, str) or isinstance(value, unicode):
+            return value
+        raise TypeError('%s cannot be formatted as XHTML' % value)
+ 
+    def render(self, name, value, attrs=None):
+        final_attrs = self.build_attrs(attrs, name=name)
+        return mark_safe(u'<textarea%s>%s</textarea>' % (
+                forms.util.flatatt(final_attrs),
+                conditional_escape(force_unicode(self._format_value(value)))))
+
--- editorsnotes/main/models.py
+++ editorsnotes/main/models.py
@@ -2,22 +2,16 @@
 
 import re
 import os.path
+import fields
 from copy import deepcopy
-from lxml import etree, html
-from lxml.html.clean import Cleaner
+from lxml import etree
 from unaccent import unaccent
 from isodate import datetime_isoformat
-from django import forms
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.safestring import mark_safe
-from django.utils.html import conditional_escape
-from django.utils.encoding import force_unicode
 
 textify = etree.XSLT(etree.parse(
         os.path.abspath(os.path.join(os.path.dirname(__file__), 'textify.xsl'))))
-
-cleaner = Cleaner(style=True)
 
 def xhtml_to_text(xhtml):
     return etree.tostring(textify(xhtml), method='text', encoding=unicode).strip()
@@ -36,54 +30,6 @@ def truncate(text, length=100):
     else:
         r = r[1]
     return l + u'... ' + r
-
-class XHTMLWidget(forms.Textarea):
-    def _format_value(self, value):
-        if value is None:
-            return ''
-        if isinstance(value, html.HtmlElement):
-            return etree.tostring(value)
-        if isinstance(value, str) or isinstance(value, unicode):
-            return value
-        raise TypeError('%s cannot be formatted as XHTML' % value)
- 
-    def render(self, name, value, attrs=None):
-        final_attrs = self.build_attrs(attrs, name=name)
-        return mark_safe(u'<textarea%s>%s</textarea>' % (
-                forms.util.flatatt(final_attrs),
-                conditional_escape(force_unicode(self._format_value(value)))))
-
-
-class XHTMLField(models.Field):
-    description = 'A parsed XHTML fragment'
-    __metaclass__ = models.SubfieldBase
-    def __init__(self, *args, **kwargs):
-        super(XHTMLField, self).__init__(*args, **kwargs)
-    def db_type(self):
-        return 'xml'
-    def to_python(self, value):
-        if isinstance(value, html.HtmlElement):
-            return value
-        if not (isinstance(value, str) or isinstance(value, unicode)):
-            raise TypeError('%s cannot be parsed to XHTML' % type(value))
-        if len(value) == 0:
-            return None
-        #value = re.sub(r'(&#13;\n)+', ' ', value)
-        fragment = None
-        try:
-            fragment = html.fragment_fromstring(value)
-        except etree.ParserError:
-            fragment = html.fragment_fromstring(value, create_parent='div')
-        return cleaner.clean_html(fragment)
-    def get_db_prep_value(self, value):
-        return etree.tostring(value)
-    def value_to_string(self, obj):
-        value = self._get_val_from_obj(obj)
-        return self.get_db_prep_value(value)
-    def formfield(self, **kwargs):
-        defaults = {'widget': XHTMLWidget}
-        defaults.update(kwargs)
-        return super(XHTMLField, self).formfield(**defaults)
 
 class CreationMetadata(models.Model):
     creator = models.ForeignKey(User, editable=False, related_name='created_%(class)s_set')
@@ -134,7 +80,7 @@ class Note(CreationMetadata):
     IntegrityError: duplicate key value violates unique constraint "main_termassignment_term_id_key"
     <BLANKLINE>
     """
-    content = XHTMLField()
+    content = fields.XHTMLField()
     type = models.CharField(max_length=1, choices=(('N','note'),('Q','query')), default='N')
     terms = models.ManyToManyField('Term', through='TermAssignment')
     last_updater = models.ForeignKey(User, editable=False, related_name='last_to_update_note_set')
@@ -162,7 +108,7 @@ class Reference(CreationMetadata):
     A bibliographic citation (as XHTML) and reference to a document.
     """
     note = models.ForeignKey(Note, related_name='references')
-    citation = XHTMLField()
+    citation = fields.XHTMLField()
     ordering = models.CharField(max_length=5, editable=False)
     url = models.URLField(blank=True, verify_exists=True)
     def citation_as_html(self):
