568f1ad9bd7fbda4d1eb8dbc9fde18e51521de61
Ryan Shaw <ryanshaw@unc.edu>
2011-04-07 14:26:44 -0400

    Got sorting by facets working.

--- editorsnotes/static/function/topic.js
+++ editorsnotes/static/function/topic.js
@@ -24,6 +24,13 @@ $(document).ready(function() {
       filter: (intersection.length == 0 ? false : intersection.join(',')) });
   }
 
+  function update_sort(facet_name, ascending) {
+    $('#document-list').isotope({ 
+      sortBy: facet_name,
+      sortAscending: ascending
+    });
+  }
+
   function init_facet(name) {
     var keys = [];
     var counts = {};
@@ -57,17 +64,50 @@ $(document).ready(function() {
     });
   }
 
-  $.each(facets, function(index, value) {
-    init_facet(value);
-  });
+  function init_sort_buttons() {
+    $('.sort-button').click(function() {
+      if ($(this).hasClass('ui-state-default')) {
+        $('.sort-button').removeClass('ui-state-highlight').addClass('ui-state-default');
+        $(this).removeClass('ui-state-default').addClass('ui-state-highlight');
+      } else { // ui-state-highlight
+        $(this).find('.ui-icon')
+          .toggleClass('ui-icon-triangle-1-s')
+          .toggleClass('ui-icon-triangle-1-n');
+      }
+      var ascending = $(this).find('.ui-icon').hasClass('ui-icon-triangle-1-s');
+      update_sort($(this).data('facet'), ascending);
+    }).hover(function() {
+      $(this).toggleClass('ui-state-hover');
+    });
+  }
+
+  var isotope_ready = false;
+  function init_isotope() {
+    if (! isotope_ready) {
+      var sort_functions = {};
+      $.each(facets, function(index, value) {
+        init_facet(value);
+        sort_functions[value] = function(element) {
+          var sortkey = element.find('.document').data(value);
+          if (sortkey == undefined) { sortkey = 'zzzzzzzzzzzzzzzzzzzzzzzzz'; }
+          return sortkey;
+        };
+      });
+      $('#document-list').isotope({
+        itemSelector: '.document-item',
+        layoutMode: 'straightDown',
+        getSortData: sort_functions
+      });
+      init_sort_buttons();
+      $('#year-sort-button').click();
+      isotope_ready = true;
+    } 
+  }
 
   $('#tabs').tabs({
     show: function(event, ui) {
       if (ui.panel.id == 'documents') {
-        $('#document-list').isotope({
-          itemSelector: '.document-item',
-          layoutMode: 'straightDown'
-        });
+        init_isotope();
       }
     },
   });
