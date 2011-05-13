efce1a94b6f49c1367b4ec1176459b40955f6988
Ryan Shaw <ryanshaw@unc.edu>
2011-05-13 13:02:26 -0400

    Fix borken footnoting tool.

--- editorsnotes/main/fields.py
+++ editorsnotes/main/fields.py
@@ -11,6 +11,21 @@ from django.utils.encoding import force_unicode
 
 cleaner = Cleaner(style=True)
 
+def update_attrs(attrs, extra_attrs):
+    if attrs is None:
+        attrs = extra_attrs
+    else:
+        for k, v in extra_attrs.iteritems():
+            if k in attrs and k in ['class', 'style']:
+                values = attrs[k].split()
+                values.extend(extra_attrs[k].split())
+                if k == 'class':
+                    attrs[k] = ' '.join(values)
+                elif k == 'style':
+                    attrs[k] = '; '.join(values)
+            else:
+                attrs[k] = extra_attrs[k]
+
 class XHTMLWidget(forms.Textarea):
     def _format_value(self, value):
         if value is None:
@@ -21,18 +36,16 @@ class XHTMLWidget(forms.Textarea):
             return value
         raise TypeError('%s cannot be formatted as XHTML' % value)
     def render(self, name, value, attrs=None):
+        update_attrs(attrs, { 'class': 'xhtml-textarea' })
         final_attrs = self.build_attrs(attrs, name=name)
-        return mark_safe(u'<textarea class="xhtml-textarea"%s>%s</textarea>' % (
+        return mark_safe(u'<textarea%s>%s</textarea>' % (
                 forms.util.flatatt(final_attrs),
                 conditional_escape(force_unicode(self._format_value(value)))))
 
 class ReadonlyXHTMLWidget(XHTMLWidget):
     def render(self, name, value, attrs=None):
-        extra_attrs = { 'style': 'display: none', 'class': 'hidden-textarea' }
-        if attrs:
-            attrs.update(extra_attrs);
-        else:
-            attrs = extra_attrs
+        update_attrs(
+            attrs, { 'style': 'display: none', 'class': 'hidden-textarea' })
         return (mark_safe(
                 '<div class="readonly">' 
                 + self._format_value(value)
