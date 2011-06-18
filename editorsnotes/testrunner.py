a02de09ca1c122f8f8f78897a0ff9616cb3a4e24
Ryan Shaw <ryanshaw@unc.edu>
2011-06-18 17:12:24 -0400

    Added custom test runner.

---
+++ editorsnotes/testrunner.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.test.simple import DjangoTestSuiteRunner
+
+# custom test runner only sets up test DB if necessary.
+class CustomTestSuiteRunner(DjangoTestSuiteRunner):
+    def run_tests(self, test_labels, extra_tests=None, **kwargs):
+        self.setup_test_environment()
+        suite = self.build_suite(test_labels, extra_tests)
+        need_database = False
+        for test in suite:
+            if type(test) is TestCase:
+                need_database = True
+                break
+        if need_database:
+            old_config = self.setup_databases()
+        result = self.run_suite(suite)
+        if need_database:
+            self.teardown_databases(old_config)
+        self.teardown_test_environment()
+        return self.suite_result(suite, result)
+
+    
