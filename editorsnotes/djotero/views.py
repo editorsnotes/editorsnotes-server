baa0198fdff63c45e32ec8b823554a48ef67c0f2
Patrick Golden <ptgolden@berkeley.edu>
2011-08-17 12:42:12 -0700

    Add ability to import documents from a topic page

--- editorsnotes/djotero/views.py
+++ editorsnotes/djotero/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
-from editorsnotes.main.models import Document
+from editorsnotes.main.models import Document, Topic, TopicAssignment
 from models import ZoteroLink
 import utils
 import json
@@ -19,6 +19,8 @@ def import_zotero(request, username=False):
         o['zotero_status'] = True
     else:
         o['zotero_status'] = False
+    o['related_object'] = request.GET.get('rel', '')
+    o['related_id'] = request.GET.get('id', '')
     return render_to_response(
         'import-zotero.html', o, context_instance=RequestContext(request))
 
@@ -47,20 +49,24 @@ def list_items(request):
     return HttpResponse(json.dumps(latest), mimetype='text/plain')
 
 def import_items(request):
+    raw = request.POST.getlist('item')
+    items = []
     o={}
-    o['raw'] = request.POST.getlist('item')
-    o['items'] = []
     o['imported_docs'] = []
     o['existing_docs'] = []
-    for item in o['raw']:
-        o['items'].append(json.loads(item,strict=False))
-    for doc_import in o['items']:
+    for item in raw:
+        items.append(json.loads(item,strict=False))
+    for doc_import in items:
         if not ZoteroLink.objects.filter(zotero_url=doc_import['url']):
             d = Document(creator=request.user, last_updater=request.user, description=doc_import['csl'])
             d.save()
             o['imported_docs'].append(d)
             link = ZoteroLink(zotero_data=json.dumps(doc_import['json']), zotero_url=doc_import['url'], doc_id=d.id)
             link.save()
+            if doc_import['related_object'] == 'topic':
+                related_topic = Topic.objects.get(id=int(doc_import['related_id']))
+                new_assignment = TopicAssignment.objects.create(content_object=d, topic=related_topic, creator=request.user)
+                new_assignment.save()
         else:
             o['existing_docs'].append(ZoteroLink.objects.filter(zotero_url=doc_import['url'])[0].doc)
     return render_to_response(
