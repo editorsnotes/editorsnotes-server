5775dec46b4d99555a6919490c9cfa707d392b3d
Patrick Golden <ptgolden@berkeley.edu>
2011-10-03 13:49:04 -0700

    Validate Zotero information before saving it in user profile.

--- editorsnotes/static/function/zotero-info.js
+++ editorsnotes/static/function/zotero-info.js
@@ -6,15 +6,25 @@ $(function() {
     width: 400
   });
   
-  $("#dialog-form").keyup(function(){
-    var keyLength = $("#zotero-key").val().length;
-    var uidLength = $("#zotero-id").val().length;
-    if (keyLength == 24 && uidLength == 6) {
-      $("#dialog-form input[type='submit']").attr('disabled', false);  
-    }
-    else {
-      $("#dialog-form input[type='submit']").attr('disabled', true);  
-    };
+  $("#test-zotero-information").live('click', function(){
+      var button = $(this).replaceWith('<img class="loading-image" src="/media/style/icons/ajax-loader.gif">');
+      $.ajax({
+        url: '/document/upload/access',
+        data: {
+          validate: "1",
+          zotero_uid: $("#zotero-id").val(),
+          zotero_key: $("#zotero-key").val()
+        },
+        success: function() {
+          $(".loading-image").remove();
+          $("#test-message").html('<p>Name and key OK!</p>');
+          $("#dialog-form input[type='submit']").attr('disabled', false).show();
+        },
+        error: function() {
+          $(".loading-image").replaceWith(button);
+          $("#test-message").html('<p>Combination invalid, make sure that the ID and Key are entered correctly and that they key provides read access.</p>');
+        }
+      });
   });
   
   $("#edit-zotero-info").click( function() {
