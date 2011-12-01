bde6e3d1b663d50bedee64d1058b4e57bcc0dce4
Patrick Golden <ptgolden@berkeley.edu>
2011-12-01 12:42:45 -0800

    Make zotero link str more descriptive

--- editorsnotes/djotero/models.py
+++ editorsnotes/djotero/models.py
@@ -8,6 +8,8 @@ class ZoteroLink(models.Model):
     zotero_url = models.URLField()
     zotero_data = models.TextField(blank=True)
     date_information = models.TextField(blank=True)
+    def __str__(self):
+        return 'Zotero data: %s' % self.doc.__str__()
     def get_zotero_fields(self):
         z = json.loads(self.zotero_data)
         z['itemType'] = utils.type_map['readable'][z['itemType']]
