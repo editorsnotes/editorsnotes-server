a2915a75361eeee4286015ca532fe1b6b6864ecc
Patrick Golden <ptgolden@berkeley.edu>
2011-12-05 12:02:34 -0800

    Improve widget for easy reading with js

--- editorsnotes/djotero/widgets.py
+++ editorsnotes/djotero/widgets.py
@@ -1,7 +1,9 @@
 import simplejson as json
 from ordereddict import OrderedDict
 from django.forms import Widget
+from django.forms.util import flatatt
 from django.utils.safestring import mark_safe
+from utils import readable_map
 
 class ZoteroWidget(Widget):
     """
@@ -9,50 +11,51 @@ class ZoteroWidget(Widget):
 
     """
     def render(self, name, value, attrs=None):
-        def pair_to_html(key, val):
-            field = """
-<span class="zotero-key">%s:&nbsp;</span>
-<textarea rows="1" cols="60" class="zotero-val" name="%s">%s</textarea><br/>
-            """ % (key, key, val)
-            return field
-        def creators_to_html(creators):
-            creator_count = 1
-            field = ''
-            add_or_remove = """
-<span class="creator-add creator-button"><img
-src="/django_admin_media/img/admin/icon_addlink.gif"></span>
-<span class="creator-remove creator-button"><img
-src="/django_admin_media/img/admin/icon_deletelink.gif"></span><br/>
-            """
-            for creator in creators:
-                c = dict(creator)
-                field += '<span class="zotero-key">%s&nbsp;</span>' % c['creatorType']
-                if c.get('name'):
-                    name_field = """
-<textarea class="zotero-val" name="creator-%s-name">%s</textarea><br/>
-                    """ % ( str(creator_count),
-                            c.get('name') )
-                else:
-                    name_field = """
-<textarea class="zotero-val" name="creator-%s-lastName">%s</textarea>, 
-<textarea class="zotero-val" name="creator-%s-firstName">%s</textarea>
-                    """ % ( str(creator_count),
-                            c.get('lastName'),
-                            str(creator_count),
-                            c.get('firstName') )
-                field += name_field
-                field += add_or_remove
-            return field
         data = json.loads(value, object_pairs_hook=OrderedDict)
-        html = '<br/><br/><div id="zotero-data">'
-        skip_fields = ['itemType', 'url']
+        html = '<br/><br/><div id="zotero-information">'
         for key, val in data.items():
-            if isinstance(val, unicode) and key not in skip_fields:
-                html += pair_to_html(key, val)
-            elif isinstance(val, list):
-                if key == 'creators':
-                    html += creators_to_html(val)
-            else:
+
+            wrapper_attrs = {'class' : 'zotero-entry',
+                             'zotero-key' : key}
+            item = ''
+
+            if key == 'itemType':
                 pass
-        html += '</form>'
+            elif key == 'creators':
+                creators = val
+                if not creators:
+                    pass
+                for creator in creators:
+                    creator_attrs = {}
+                    creator_attrs['class'] = 'zotero-creator'
+                    creator_attrs['creator-type'] = creator['creatorType']
+
+                    creator_html = ''
+                    creator_html += '<label>%s</label>&nbsp;' % creator['creatorType']
+                    if creator.get('name'):
+                        creator_html += '<textarea%s>%s</textarea>' % (
+                            ' creator-key="name"', creator['name']
+                        )
+                    else:
+                        creator_html += '<textarea%s>%s</textarea>, ' % (
+                            ' creator-key="lastName"', creator['lastName']
+                        )
+                        creator_html += '<textarea%s>%s</textarea>' % (
+                            ' creator-key="firstName"', creator['firstName']
+                        )
+                    item += '<span%s>%s</span><br/>' % (
+                        flatatt(creator_attrs),creator_html
+                    )
+            elif key == 'tags':
+                pass
+            elif isinstance(val, unicode) :
+                item += '<label>%s</label>&nbsp;' % (key)
+                item += '<textarea%s>%s</textarea><br/>' % ('', val)
+
+            if item:
+                html += '<span%s>%s</span>' % (flatatt(wrapper_attrs), item)
+        html += '</div>'
         return mark_safe(html)
+
+    def get_creators(self):
+        pass
