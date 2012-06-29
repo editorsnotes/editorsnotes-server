51610297ced2f983eb2fd6c82c95ae36931288c1
Patrick Golden <ptgolden@berkeley.edu>
2012-06-29 11:11:28 -0700

    Improve js faceting plugin

--- editorsnotes/static/function/facets.js
+++ editorsnotes/static/function/facets.js
@@ -1,181 +1,218 @@
 (function($) {
 
-  // Build the container for the facets
-  var methods = {
-    init: function() {
-      return this.each(function(){
-        var container = $(this),
-          facetFields = ['itemtype', 'publicationtitle',
-                         'author', 'recipient', 'archive'],
-          allItems = container.find('.document'),
-          initialFacets = facetObject(allItems, facetFields);
-        container.data('facetFields', facetFields);
-        container.data('allItems', allItems);
-        container.data('initialFacets', initialFacets);
+  $.fn.facet = function(method) {
+    /*
+     * Builds a form to facet items based on data attributes of elements. Accepts
+     * either strings or arrays of strings as values.
+     *
+     * Usage:
+     * $someContainer.facet({
+     *   fields = [ {'key': 'facetA', 'label': 'First Facet Label'},
+     *              {'key': 'facetB', 'label': 'Second Facet Label'}, ... ]
+     *   itemSelector = '.elements-with-data-atts'
+     * });
+     *
+     */
+    var methods = {
 
-        $.each(facetFields, function(idx, key) {
-          if (initialFacets[key]) {
-            // Button to select this filter
-            $('<li class="document-facet btn">')
-              .data('for', key)
-              .html('<i class="icon-plus"></i>' + key)
-              .appendTo($selectors)
+      init: function( options ) {
 
-              // List of values for this filter
-             $('<div class="facet-container span10">')
-                .hide()
-                .addClass(key + '-facet-container')
-                .html('<h6>' + key + '</h6>')
-                .appendTo($values)
-                .append('<ul class="unstyled">');
-          }
-        });
+        if (!options.fields) {
+          console.log('Provide a list of fields to facet on');
+          return;
+        }
+        if (!options.itemSelector) {
+          console.log('Provide a selector for the items to be faceted.');
+          return;
+        }
 
-        $facetContainer.insertBefore(container);
+        return this.each(function(){
+          var container = $(this),
+            facetFields = options.fields;
+            allItems = container.find(options.itemSelector),
+            initialFacets = facetObject(allItems, facetFields);
+          container.data('facetFields', facetFields);
+          container.data('allItems', allItems);
+          container.data('initialFacets', initialFacets);
 
-        $('.facet-input').live('click', function() {
-          container.facet('update')
-        });
-        $('.document-facet').live('click', function() {
-          var $filter = $(this),
-            criteria = $filter.data('for'),
-            target = $('.' + criteria + '-facet-container').toggle();
-          $filter
-            .toggleClass('document-facet-selected btn-primary')
-            .find('i')
-              .toggleClass('icon-plus icon-minus icon-white');
-          if (!$filter.hasClass('document-facet-selected')) {
-            target.find('input')
-              .prop('checked', false)
-              .filter(':first')
-              .trigger('click');
-          }
-        });
+          // The container for the facets
+          var $facetContainer = $('' +
+            '<div id="facets" class="row">' +
+              '<div id="facet-heading" class="span12"></div>' +
+              '<div id="facet-container">' +
+                '<div class="span2" id="available-facets">' +
+                  '<ul class="unstyled">' +
+                  '</ul>' +
+                '</div>' +
+                '<div class="span10 roow" id="facet-inputs">' +
+                '</div>' +
+              '</div>' +
+            '</div>'),
+            $heading = $facetContainer.find('#facet-heading');
+            $container = $facetContainer.find('#facet-container'),
+            $selectors = $facetContainer.find('ul'),
+            $values = $facetContainer.find('#facet-inputs');
 
-        container.facet('update');
-      });
-    },
+          // Show/hide the container
+          $('<h3 style="cursor: pointer;" id="facet-title">')
+            .html('Filters <i style="vertical-align: baseline;" class="icon-minus"></i>')
+            .bind('click', function() {
+              $(this).find('i').toggleClass('icon-plus icon-minus');
+              $container.toggle();
+            }).appendTo($heading);
 
-    update: function() {
-      return this.each(function(){
 
-        var container = $(this),
-          items = container.data('allItems'),
-          facetFields = container.data('facetFields'),
-          facets = facetObject(items, facetFields),
-          itemsToShow = [],
-          checkedInputs = $('.facet-input:checked'),
-          appliedFacets = checkedInputs.map(function() {
-            return [ facets[this.name][this.value] ];
-          });
+          $.each(facetFields, function() {
+            var facetKey = this.key,
+              facetLabel = this.label;
+            if (initialFacets[facetKey]) {
+              // Button to select this filter
+              $('<li class="document-facet btn">')
+                .data('for', facetKey)
+                .html('<i class="icon-plus"></i>' + facetLabel)
+                .appendTo($selectors)
 
-          // Show/hide items
-          if (!appliedFacets.length) {
-            items.show();
-          } else {
-            var comparisons = appliedFacets.slice(1);
-            $.each(appliedFacets[0], function(idx, item) {
-              var inTheRest = true;
-              $.each(comparisons, function() {
-                if (this.indexOf(item) == -1) {
-                  inTheRest = false;
-                }
-              });
-              if (inTheRest) {
-                itemsToShow.push(item);
-              }
-            });
-            items.hide();
-            $(itemsToShow).show();
-          }
-
-          facets = facetObject(items.filter(':visible'), facetFields),
-        
-          // Inputs from facets
-          $.each(facetFields, function(idx, facetKey) {
-            var $filterList = $('.' + facetKey + '-facet-container ul'),
-              thisFacet = facets[facetKey] || [],
-              facetLength = 0;
-            $filterList.children().remove();
-            $.each(thisFacet, function(item, arr) {
-              facetLength += 1;
-              var $item = $('<li>')
-                .appendTo($filterList)
-                .html('&nbsp;' + item + ' (' + arr.length + ')')
-              $('<input class="facet-input" type="checkbox">')
-                .attr('name', facetKey)
-                .val(item)
-                .prependTo($item)
-            });
-            if (!facetLength) {
-              $('<span class="quiet">(no values)</span>').appendTo($filterList);
+                // List of values for this filter
+               $('<div class="facet-container span10">')
+                  .hide()
+                  .addClass(facetKey + '-facet-container')
+                  .html('<h6>' + facetLabel + '</h6>')
+                  .appendTo($values)
+                  .append('<ul class="unstyled">');
             }
           });
-          
-          $.each(checkedInputs, function() {
-            $('.facet-input[name="' + this.name + '"][value="' + this.value + '"]')
-              .prop('checked', true);
+
+          $facetContainer.insertBefore(container);
+
+          $('.facet-input').live('click', function() {
+            container.facet('update')
+          });
+          $('.document-facet').live('click', function() {
+            var $filter = $(this),
+              criteria = $filter.data('for'),
+              target = $('.' + criteria + '-facet-container').toggle();
+            $filter
+              .toggleClass('document-facet-selected btn-primary')
+              .find('i')
+                .toggleClass('icon-plus icon-minus icon-white');
+            if (!$filter.hasClass('document-facet-selected')) {
+              target.find('input')
+                .prop('checked', false)
+                .filter(':first')
+                .trigger('click');
+            }
           });
 
-      });
-    }
-  },
+          container.facet('update');
+        });
+      },
 
-  facetObject = function(items, facets) {
-    var f = {};
-    f.fields = {};
-    f.pushFacetValue = function(field, k, v) {
-      // Default values for undefined keys
-      f.fields[field] = f.fields[field] || {};
-      f.fields[field][k] = f.fields[field][k] || [];
-      f.fields[field][k].push(v);
-    };
-    $.each(items, function() {
-      var $this = $(this),
-        data = $this.data();
-      $.each(facets, function() {
-        var facetKey = this;
-        if (data.hasOwnProperty(facetKey)){
-          var facetVal = data[facetKey];
-          if (!typeof(facetVal) == 'string') {
-            $.each(facetVal, function() {
-              f.pushFacetValue(facetKey, this, $this[0]);
+      // Update form to reflect the selected inputs
+      update: function() {
+        return this.each(function(){
+
+          var container = $(this),
+            items = container.data('allItems'),
+            facetFields = container.data('facetFields'),
+            facets = facetObject(items, facetFields),
+            itemsToShow = [],
+            checkedInputs = $('.facet-input:checked'),
+            appliedFacets = checkedInputs.map(function() {
+              return [ facets[this.name][this.value] ];
             });
-          } else {
-            f.pushFacetValue(facetKey, facetVal, $this[0]);
+
+            // Show/hide items before creating new facet values
+            if (!appliedFacets.length) {
+              // Show all items if not facets are selected
+              items.show();
+            } else {
+              // Otherwise, find all items that match the given criteria.
+              // There may be a better way to do this, but for now, this looks
+              // at the first field faceted on, finds the items that match
+              // that restraint, and then checks each of the other applied
+              // restraints, hiding all elements that don't apply.
+              //
+              // In effect this is just doing a union of the different arrays
+              // of items generated by the facet criteria.
+
+              var comparisons = appliedFacets.slice(1);
+              $.each(appliedFacets[0], function(idx, item) {
+                var inTheRest = true;
+                $.each(comparisons, function() {
+                  if (this.indexOf(item) == -1) {
+                    inTheRest = false;
+                  }
+                });
+                if (inTheRest) {
+                  itemsToShow.push(item);
+                }
+              });
+              items.hide();
+              $(itemsToShow).show();
+            }
+
+            facets = facetObject(items.filter(':visible'), facetFields),
+          
+            // Inputs from facets
+            $.each(facetFields, function() {
+              var facetKey = this.key,
+                $filterList = $('.' + facetKey + '-facet-container ul'),
+                thisFacet = facets[facetKey] || [],
+                facetLength = 0;
+              $filterList.children().remove();
+              $.each(thisFacet, function(item, arr) {
+                facetLength += 1;
+                var $item = $('<li>')
+                  .appendTo($filterList)
+                  .html('&nbsp;' + item + ' (' + arr.length + ')')
+                $('<input class="facet-input" type="checkbox">')
+                  .attr('name', facetKey)
+                  .val(item)
+                  .prependTo($item)
+              });
+              if (!facetLength) {
+                $('<span class="quiet">(no values)</span>').appendTo($filterList);
+              }
+            });
+            
+            $.each(checkedInputs, function() {
+              $('.facet-input[name="' + this.name + '"][value="' + this.value + '"]')
+                .prop('checked', true);
+            });
+
+        });
+      }
+    };
+
+    var facetObject = function(items, facetFields) {
+      var f = {};
+      f.fields = {};
+      f.pushFacetValue = function(field, k, v) {
+        // Default values for undefined keys
+        f.fields[field] = f.fields[field] || {};
+        f.fields[field][k] = f.fields[field][k] || [];
+        f.fields[field][k].push(v);
+      };
+      $.each(items, function() {
+        var $this = $(this),
+          data = $this.data();
+        $.each(facetFields, function() {
+          var facetKey = this.key;
+          if (data.hasOwnProperty(facetKey)){
+            var facetVal = data[facetKey];
+            if (!typeof(facetVal) == 'string') {
+              $.each(facetVal, function() {
+                f.pushFacetValue(facetKey, this, $this[0]);
+              });
+            } else {
+              f.pushFacetValue(facetKey, facetVal, $this[0]);
+            }
           }
-        }
+        });
       });
-    });
-    return f.fields;
-  },
+      return f.fields;
+    };
 
-  $facetContainer = $('' +
-    '<div id="facets" class="row">' +
-      '<div id="facet-heading" class="span12"></div>' +
-      '<div id="facet-container">' +
-        '<div class="span2" id="available-facets">' +
-          '<ul class="unstyled">' +
-          '</ul>' +
-        '</div>' +
-        '<div class="span10 roow" id="facet-inputs">' +
-        '</div>' +
-      '</div>' +
-    '</div>'),
-    $heading = $facetContainer.find('#facet-heading');
-    $container = $facetContainer.find('#facet-container'),
-    $selectors = $facetContainer.find('ul'),
-    $values = $facetContainer.find('#facet-inputs');
-
-    $('<h3 style="cursor: pointer;" id="facet-title">')
-      .html('Filters <i style="vertical-align: baseline;" class="icon-minus"></i>')
-      .bind('click', function() {
-        $(this).find('i').toggleClass('icon-plus icon-minus');
-        $container.toggle();
-      }).appendTo($heading);
-
-
-  $.fn.facet = function(method) {
 
     if (methods[method]) {
       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
