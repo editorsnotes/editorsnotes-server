2752cde1ef6f84b75792996efdf93b37bfcd5543
Patrick Golden <ptgolden@berkeley.edu>
2012-07-18 16:45:17 -0700

    Create base Cluster model & DocumentCluster
    Base model includes creation metadata and an optional message

--- editorsnotes/refine/models.py
+++ editorsnotes/refine/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-from editorsnotes.main.models import Topic
+from django.contrib.auth.models import User
+from editorsnotes.main.models import Topic, Document
 
-# Create your models here.
+class BaseCluster(models.Model):
+    creator = models.ForeignKey(User, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    marked_for_merge = models.BooleanField(default=False)
+    message = models.TextField(blank=True, null=True)
+    class Meta:
+        abstract = True
 
-class TopicCluster(models.Model):
-    clustered_topics = models.ManyToManyField(Topic)
+class TopicCluster(BaseCluster):
+    topics = models.ManyToManyField(Topic, related_name='in_cluster')
+
+class DocumentCluster(BaseCluster):
+    documents = models.ManyToManyField(Document, related_name='in_cluster')
