6e4c4fc8ef6515c5693d51f4719be50fa265e3e0
Patrick Golden <ptgolden@berkeley.edu>
2012-07-20 10:55:56 -0700

    Change clusters' URL schemes
    Allow urls for both topics and documents & add model permalinks

--- editorsnotes/refine/models.py
+++ editorsnotes/refine/models.py
@@ -8,14 +8,23 @@ class BaseCluster(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     marked_for_merge = models.BooleanField(default=False)
     message = models.TextField(blank=True, null=True)
+    @models.permalink
+    def get_absolute_url(self):
+        return ('%s_view' % self._meta.module_name, [str(self.id)])
     class Meta:
         abstract = True
 
 class TopicCluster(BaseCluster):
     topics = models.ManyToManyField(Topic, related_name='in_cluster')
+    @models.permalink
+    def get_absolute_url(self):
+        return ('merge_topic_cluster_view', [str(self.id)])
 
 class DocumentCluster(BaseCluster):
     documents = models.ManyToManyField(Document, related_name='in_cluster')
+    @models.permalink
+    def get_absolute_url(self):
+        return ('merge_document_cluster_view', [str(self.id)])
 
 class BadClusterPair(models.Model):
     content_type = models.ForeignKey(ContentType)
