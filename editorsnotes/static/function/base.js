dd308408abe41cb8c88df98e0bbfa5a22a1d3065
Patrick Golden <ptgolden@berkeley.edu>
2012-07-24 17:53:59 -0700

    Extend base autocomplete JS functionality
    1. Enables autocomplete fields for notes and documents (not just topics)
    2. Truncates results over 100 characters
    3. Allows for selection of a result without redirection. If the
    autocomplete field has the class "autocomplete-no-redirect", once an
    item is selected, the field will be disabled, and two hidden inputs with
    the model type & object id will be appended following the field.

--- editorsnotes/static/function/base.js
+++ editorsnotes/static/function/base.js
@@ -7,6 +7,18 @@ $(document).ready(function () {
     $('#bottom-nav').toggle();
   }); 
 
+  var truncateChars = function(text, l) {
+    if (typeof(l) == 'undefined') {
+      var l = 100;
+    }
+    if (text.length > l) {
+      return text.substr(0, l/2) + ' ... ' + text.substr(-(l/2));
+    }
+    else {
+      return text;
+    }
+  }
+
   // Initialize autocomplete for search input box.
   $('input.search-autocomplete')
   .keydown(function(event) {
@@ -18,22 +30,66 @@ $(document).ready(function () {
     }
   })
   .autocomplete({
-    source: function(request, response) {
-      var targetModel = this.element.attr('search-target');
-      if (targetModel == 'topics') {
-        $.getJSON('/api/topics/', { q: request.term }, function(data) {
-          response($.map(data, function(item, index) {
-            return { label: item.preferred_name, uri: item.uri };
-          }));
-        });
-      } else {
-        // TODO: other models search complete
+    source: function(request, response, x) {
+      var targetModel = this.element.attr('search-target'),
+        query = {'q': request.term};
+
+      if (this.element.attr('search-project-id')) {
+        query['project_id'] = this.element.attr('search-project-id');
+      }
+
+      switch (targetModel) {
+        case 'topics':
+          $.getJSON('/api/topics/', query, function(data) {
+            response($.map(data, function(item, index) {
+              return { id: item.id, label: truncateChars(item.preferred_name), uri: item.uri };
+            }));
+          });
+          break;
+
+        case 'notes':
+          $.getJSON('/api/notes/', query, function(data) {
+            response($.map(data, function(item, index) {
+              return { id: item.id, label: truncateChars(item.title), uri: item.uri };
+            }));
+          })
+          break;
+
+        case 'documents':
+          $.getJSON('/api/documents/', query, function(data) {
+            response($.map(data, function(item, index) {
+              return { id: item.id, label: truncateChars(item.description), uri: item.uri };
+            }));
+          })
+          break;
       }
     },
     minLength: 2,
     select: function(event, ui) {
-      if (ui.item) {
+      var $this = $(event.target);
+      if (ui.item && !$this.hasClass('autocomplete-no-redirect')) {
         location.href = ui.item.uri;
+      } else if (ui.item) {
+        $this.attr('disabled', 'disabled');
+        $('<input type="hidden">').attr({
+          'name': 'autocomplete-model',
+          'value': $this.attr('search-target')
+        }).insertAfter($this);
+        $('<input type="hidden">').attr({
+          'name': 'autocomplete-id',
+          'value': ui.item.id
+        }).insertAfter($this);
+
+        $('<i class="icon-remove-sign clear-search">')
+          .insertAfter($this)
+          .click(function() {
+            var $this = $(this);
+            $this.prev('input.search-autocomplete')
+              .removeAttr('disabled')
+              .val('')
+              .siblings('input[type="hidden"][name^="autocomplete-"]').remove();
+            $this.remove();
+          });
       }
     }
   });
