b529019bb1610c8ae38c3599d1f9d5e5c6ab4d9c
Patrick Golden <ptgolden@berkeley.edu>
2012-09-06 20:06:26 -0700

    New widget for scans that accepts multiple files
    If a user's browser allows it, the file input of the 'extra' field in
    a scan formset will contain the attribute "multiple", allowing the user
    to upload multiple images at once. Everything will work as before if
    that is not supported.

--- editorsnotes/main/fields.py
+++ editorsnotes/main/fields.py
@@ -26,6 +26,16 @@ def update_attrs(attrs, extra_attrs):
             else:
                 attrs[k] = extra_attrs[k]
 
+class MultipleFileInput(forms.ClearableFileInput):
+    def __init__(self, *args, **kwargs):
+        self.template_with_initial = '%(initial)s'
+        super(MultipleFileInput, self).__init__(*args, **kwargs)
+    def render(self, name, value, attrs=None):
+        attrs = attrs if attrs else {}
+        if value is None:
+            attrs['multiple'] = 'multiple'
+        return super(MultipleFileInput, self).render(name, value, attrs=attrs)
+
 class XHTMLWidget(forms.Textarea):
     def _format_value(self, value):
         if value is None:
--- editorsnotes/main/forms.py
+++ editorsnotes/main/forms.py
@@ -4,7 +4,7 @@ from django.forms.models import inlineformset_factory, modelformset_factory, Mod
 from django.contrib.auth.models import User, Group
 from django.shortcuts import get_object_or_404
 from django.utils.safestring import mark_safe
-from fields import XHTMLWidget
+from fields import XHTMLWidget, MultipleFileInput
 import models as main_models
 from models import Project, NoteSection, Document
 from editorsnotes.djotero.widgets import ZoteroWidget
@@ -122,8 +122,83 @@ class DocumentLinkForm(ModelForm):
 DocumentLinkFormset = inlineformset_factory(
     main_models.Document, main_models.DocumentLink, form=DocumentLinkForm, extra=1)
 
+class ScanForm(ModelForm):
+    class Meta:
+        fields = ('image', 'ordering')
+        model = main_models.Scan
+        widgets = {
+            'image': MultipleFileInput(),
+            'ordering': forms.HiddenInput()
+        }
+
+class ScanFormset(forms.models.BaseInlineFormSet):
+    """
+    If multiple images were posted from a single file input with the attribute
+    "multiple", this formset will split them up into regular forms
+    """
+    def _construct_forms(self):
+        self.forms = []
+        forms_from_multiple_input = []
+
+        for i in xrange(self.total_form_count()):
+            form = self._construct_form(i)
+
+            # Only pay attention to multiple files from an input if this is the
+            # last image field and there is something to save
+            if (i < self.total_form_count() - 1 or not form.files):
+                self.forms.append(form)
+                continue
+
+            # If only one image was posted from this input, treat it as a normal
+            # one. This allows us to treat this formset as it normally would be,
+            # which is beneficial for several reasons, including continued
+            # support for browsers that don't support the "multiple" attribute.
+            images = form.files.getlist('%s-image' % form.prefix) 
+            if len(images) == 1:
+                self.forms.append(form)
+                continue
+
+            # Make a copy of the file & data dictionaries, which will be amended
+            # in order to make new ScanForm instances
+            newfiles = dict((k, v) for k, v in form.files.items())
+            newdata = dict((k, v) for k, v in form.data.items())
+
+            # Pretend that each image was posted individually
+            j = i
+            for image in images:
+                prefix = self.add_prefix(j)
+                newfiles['%s-image' % prefix] = image
+                newdata['%s-ordering' % prefix] = j
+                newform = self._construct_form(j, files=newfiles, data=newdata)
+                forms_from_multiple_input.append(newform)
+                j += 1
+
+        # Only add these new forms to the formset if the images are valid. This
+        # does go against the normal flow of validation, but it makes the
+        # workflow for adding scans *much* less complicated to take an
+        # all-or-nothing approach to accepting multiple images.
+        if all(map(lambda f: f.is_valid(), forms_from_multiple_input)):
+            self.forms += forms_from_multiple_input
+        else:
+            # Make the last form an empty one, like it was previously
+            self.forms.append(
+                self._construct_form(self.total_form_count() - 1, files=None))
+            self.bad_multi_images = 'One or more files uploaded were not valid images.'
+
+        # Does the management form need to be changed at all? I can't think of
+        # any situation where it would be, but here's how it probably would be
+        # done if needed.
+        # self.management_form['initial']['TOTAL_FORMS'] = len(self.forms)
+            
+    # Display errors from a multiple upload field as a non-form error-- in the
+    # formset, not the individual form
+    def clean(self):
+        if hasattr(self, 'bad_multi_images'):
+            raise forms.ValidationError(self.bad_multi_images)
+
 ScanFormset = inlineformset_factory(
-    main_models.Document, main_models.Scan, extra=3)
+    main_models.Document, main_models.Scan,
+    form=ScanForm, formset=ScanFormset, extra=1)
 
 ################################################################################
 # Note form and formsets
