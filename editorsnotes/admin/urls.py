19c49af137a5a0ec1ec6e5da465858f6a0b92eb2
Patrick Golden <ptgolden0@gmail.com>
2012-11-29 12:11:13 -0800

    Fix error in admin urls.py

--- editorsnotes/admin/urls.py
+++ editorsnotes/admin/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^main/project/(?P<project_id>\d+)/$', 'change_project', name='project_edit_view'),
-    url(r'^main/project/(?P<project_id>\d+)/roster/$', 'project_roster', name='project_roster_view'),
-    url(r'^main/project/(?P<project_id>\d+)/featured_items/$', 'change_featured_items', name='change_featured_items_view'),
+    url(r'^main/project/(?P<project_id>\d+)/$', views.change_project, name='project_edit_view'),
+    url(r'^main/project/(?P<project_id>\d+)/roster/$', views.project_roster, name='project_roster_view'),
+    url(r'^main/project/(?P<project_id>\d+)/featured_items/$', views.change_featured_items, name='change_featured_items_view'),
     url(r'^main/topic/add/$', views.TopicAdminView.as_view(), name='admin:main_topic_add'),
     url(r'^main/topic/(?P<topic_id>\d+)/$', views.TopicAdminView.as_view(), name='admin:main_topic_change'),
     url(r'^main/document/add/$', views.DocumentAdminView.as_view(), name='admin:main_document_add'),
