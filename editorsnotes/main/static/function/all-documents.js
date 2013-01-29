de1cc4bbe138730cacbc14ac09865ce9efafabca
Patrick Golden <ptgolden0@gmail.com>
2013-01-29 15:37:53 -0800

    Make faceting on document work with less
    Change things around too

--- editorsnotes/main/static/function/all-documents.js
+++ editorsnotes/main/static/function/all-documents.js
@@ -1,78 +1,76 @@
 $(function() {
-  
-  // Toggle filter menu
-  $('#facet-title').live('click', function() {
-    $(this).find('i').toggleClass('icon-plus icon-minus');
-    $('#filter-selection').toggle();
-  });
+  var $facets = $('#facets');
+  var selectedFacets = []
 
+  $('body')
+    .on('click', '#facets.loaded .facets-available li', function () {
+      var $this = $(this).toggleClass('facet-selected')
+        , criteria = $this.data('facet-toggle')
+        , $target = $('.select-facets-' + criteria, $facets);
 
-  // Add filter criteria
-  $('#filters.loaded .document-filter').live('click', function() {
-    var $filter = $(this).toggleClass('document-filter-selected btn-primary'),
-      criteria = $filter.attr('for'),
-      target = $('.' + criteria + '-filter').toggle();
-    if (!target.is(':visible')) {
-      target.find('input:checked')
-        .prop('checked', false)
-        .filter(':first')
-          .trigger('click');
-    }
-    $filter
-      .find('i').toggleClass('icon-plus icon-minus icon-white');
-  });
-
-
-  // Apply selected filter criteria
-  $('#filter-form input').live('click', function() {
-    var $inputs = $('#filter-form input'),
-      $checkedInputs = $inputs.filter(':checked'),
-      $selectedFilters = $('.document-filter-selected'),
-      queryParams = {};
-    
-    // Build the query to be passed
-    $.each($checkedInputs, function() {
-      if (!queryParams.hasOwnProperty(this.name)) {
-        queryParams[this.name] = [];
-      }
-      queryParams[this.name].push(this.value);
-    });
-
-    // Lock inputs until request can be processed
-    $inputs.prop('disabled', true);
-    $('#filters').removeClass('loaded');
-
-    // Ajax request for results
-    $.ajax({
-      type: 'GET',
-      url: '?filter=1',
-      data: queryParams,
-      success: function(data) {
-
-        $('#all-documents').html(data);
-
-        // Restore previously selected filters
-        $.each($selectedFilters, function() {
-          var filterCriteria = $(this).attr('for');
-          $('#available-filters li[for="' + filterCriteria + '"]')
+      $target.toggle();
+      if ($this.hasClass('facet-selected')) {
+        if (selectedFacets.indexOf(criteria) === -1) {
+          selectedFacets.push(criteria);
+        }
+      } else {
+        selectedFacets.pop(criteria);
+        $('input:checked', $target)
+          .prop('checked', false)
+          .filter(':first')
             .trigger('click');
-        });
-
-        // Restore previously checked boxes
-        $.each($checkedInputs, function() {
-          var selector = '#filter-form input' +
-            '[name="' + this.name + '"]' +
-            '[value="' + this.value + '"]';
-          $(selector).prop('checked', true);
-        });
-
-      },
-      error: function() {
-        $inputs.prop('disabled', false);
-        alert('Error processing query');
       }
-    });
+      $('i', $this).toggleClass('icon-plus icon-minus');
+    })
+    .on('click', '#facets .facet-header', function () {
+      $('i', this).toggleClass('icon-plus icon-minus');
+      $('.facet-body').toggle();
+    })
+    .on('click', '#facets .facet-form input', function () {
+      var $inputs = $('input', $facets)
+        , $checkedInputs = $inputs.filter(':checked')
+        , queryParams = {}
 
-  });
+      $.each($checkedInputs, function() {
+        if (!queryParams.hasOwnProperty(this.name)) {
+          queryParams[this.name] = [];
+        }
+        queryParams[this.name].push(this.value);
+      });
+
+      $inputs.prop('disabled', true);
+      $facets.removeClass('loaded');
+
+      $.ajax({
+        type: 'GET',
+        url: '?filter=1',
+        data: queryParams,
+        success: function(data) {
+          var newdoc = $('#all-documents').html(data)
+            , prevSelectedFacets = selectedFacets.slice(0);
+
+          $facets = $('#facets', newdoc);
+
+          // Restore previously selected facets
+          console.log(prevSelectedFacets);
+          for (var i = 0; i < prevSelectedFacets.length; i++) {
+            var selector = 'li[data-facet-toggle="' + prevSelectedFacets[i] + '"]'
+            $(selector, $facets).trigger('click');
+          }
+
+          // Restore previously checked boxes
+          $checkedInputs.each(function() {
+            var selector = '[name="' + this.name + '"][value="' + this.value + '"]'
+            $('input' + selector, $facets).prop('checked', true)
+          });
+
+        },
+        error: function() {
+          $inputs.prop('disabled', false);
+          alert('Error processing query');
+        }
+      });
+
+    });
 
 });
--- editorsnotes/main/templatetags/facet.py
+++ editorsnotes/main/templatetags/facet.py
@@ -20,25 +20,40 @@ class FacetNode(template.Node):
         self.facet_name = facet_item_list[facet_item_list.rindex('.') + 1:]
         self.facet_item_list = template.Variable(facet_item_list)
     def render(self, context):
-        html = ''
-        html += '<h6>%s</h6>' % (self.facet_title)
-        facet_inputs = []
-        for val, label, count in self.facet_item_list.resolve(context):
-            facet_inputs.append('<li><input type="checkbox" name="%s" value="%s">&nbsp;%s (%s)</li>' % (
-                self.facet_name, val, label, count))
-        if len(facet_inputs) > 5:
-            col_length = len(facet_inputs) / 2
+        templates = {
+            'title': u'<h5>{}</h5>',
+            'input': (
+                u'<li>' +
+                '<input id="facet-{name}-{counter}" type="checkbox" name="{name}" value="{value}">' +
+                '<label for="facet-{name}-{counter}">{label} ({count})</label>' +
+                '</li>'
+            ),
+            'list': u'<ul class="facet-list">{}</ul>'
+        }
 
-            html += '<ul class="unstyled span5">'
-            html += '\n'.join(facet_inputs[:col_length])
-            html += '</ul>'
+        html = u'<div class="select-facets-{}">'.format(self.facet_name)
+        html += templates['title'].format(self.facet_title)
 
-            html += '<ul class="unstyled span5">'
-            html += '\n'.join(facet_inputs[col_length:])
-            html += '</ul>'
+        try:
+            facet = self.facet_item_list.resolve(context)
+            facet_inputs = []
+            counter = 0
+            for value, label, count in facet:
+                facet_inputs.append(templates['input'].format(
+                    name=self.facet_name, value=value, label=label, count=count,
+                    counter=counter))
+                counter += 1
 
-        else:
-            html += '<ul class="unstyled">'
-            html += '\n'.join(facet_inputs)
-            html += '</ul>'
+            if len(facet_inputs) > 5:
+                # We want the first column to be the longer one if there are an odd
+                # number of facets
+                col_length = len(facet_inputs) / 2 + len(facet_inputs) % 2
+                html += templates['list'].format('\n'.join(facet_inputs[:col_length]))
+                html += templates['list'].format('\n'.join(facet_inputs[col_length:]))
+            else:
+                html += templates['list'].format('\n'.join(facet_inputs))
+        except template.VariableDoesNotExist:
+            html += u'<p>No values</p>'
+
+        html += u'</div>'
         return html
