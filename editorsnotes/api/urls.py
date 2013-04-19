c147e0f11caf6d1b622332b499160c3c4d60e672
Patrick Golden <ptgolden0@gmail.com>
2013-04-19 13:50:52 -0700

    Fix typos in api urls.py

--- editorsnotes/api/urls.py
+++ editorsnotes/api/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns(
     url('^topics/$', views.TopicList.as_view(), name='api-topics-list'),
     url('^topics/(?P<pk>\d+)/$', views.TopicDetail.as_view(), name='api-topics-detail'),
     url('^notes/$', views.NoteList.as_view(), name='api-notes-list'),
-    url('^notes/(?P<pk>\d+)/$', views.NoteDetail.as_view(), name='api-notes-list'),
+    url('^notes/(?P<pk>\d+)/$', views.NoteDetail.as_view(), name='api-notes-detail'),
     url('^notes/(?P<note_id>\d+)/s(?P<section_id>\d+)/$', views.NoteSectionDetail.as_view(), name='api-notes-section-detail'),
     url('^documents/$', views.DocumentList.as_view(), name='api-documents-list'),
-    url('^documents/(?P<pk>\d+)/$', views.DocumentDetail.as_view(), name='api-documents-list'),
+    url('^documents/(?P<pk>\d+)/$', views.DocumentDetail.as_view(), name='api-documents-detail'),
 )
