109d26f0d328d523d1c3ec043f81e13ef38666f6
Patrick Golden <ptgolden0@gmail.com>
2013-11-20 01:18:26 -0500

    Make default license 'all rights reserved'

--- editorsnotes/main/models/auth.py
+++ editorsnotes/main/models/auth.py
@@ -126,7 +126,7 @@ class Project(models.Model, URLAccessible, ProjectPermissionsMixin):
     slug = models.SlugField(help_text='Used for project-specific URLs and groups')
     image = models.ImageField(upload_to='project_images', blank=True, null=True)
     description = fields.XHTMLField(blank=True, null=True)
-    default_license = models.ForeignKey(License, default=6)
+    default_license = models.ForeignKey(License, default=1)
     objects = ProjectManager()
     class Meta:
         app_label = 'main'
