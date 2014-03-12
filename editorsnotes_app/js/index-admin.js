e98f52482f73d3b70b015b957e8230d5f65f5ca5
Patrick Golden <ptgolden0@gmail.com>
2014-03-12 12:50:43 -0400

    Fix the $.editText function
    I think we're back where we should be?

--- editorsnotes_app/js/index-admin.js
+++ editorsnotes_app/js/index-admin.js
@@ -1,11 +1,29 @@
 "use strict";
 
-var EditorsNotes = window.EditorsNotes;
+var EditorsNotes = window.EditorsNotes
+  , $ = require('jquery')
+  , Editor = require('./utils/text_editor')
 
-EditorsNotes.Views = {};
-EditorsNotes.Models = {};
-EditorsNotes.Templates = {};
+EditorsNotes.Models = {
+  Project: require('./models/project')
+}
+EditorsNotes.Views = {
+  Note: require('./views/note')
+}
 
-EditorsNotes.Templates['wysihtml5_toolbar'] = require('./templates/wysihtml5_toolbar.html');
-EditorsNotes.Models.Project = require('./models/project');
-EditorsNotes.Views.Note = require('./views/note');
+$.fn.editText = function (method) {
+  var editor = this.data('editor');
+
+  if (this.length !== 1) $.error('One at a time :(');
+
+  if ( typeof(method) === 'object' || !method  ) {
+    if (editor) return this;
+    return this.data('editor', new Editor(this, method || {}));
+  } else if ( !editor ) {
+    $.error('Must initialize editor first.');
+  } else if ( Editor.prototype[method] ) {
+    return editor[method].apply(editor, Array.prototype.slice.call(arguments, 1));
+  } else {
+    $.error('No such method: ' + method);
+  }
+}
