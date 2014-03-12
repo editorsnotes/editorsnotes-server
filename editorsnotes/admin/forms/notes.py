28a12f47a45bc6d01d0d57440fa4b5d3754b0604
Patrick Golden <ptgolden0@gmail.com>
2014-03-12 14:50:57 -0400

    Remove unused note_section admin files

--- editorsnotes/admin/forms/notes.py
+++ editorsnotes/admin/forms/notes.py
@@ -14,12 +14,3 @@ class NoteForm(ModelForm):
         widgets = {
             'assigned_users': forms.CheckboxSelectMultiple()
         }
-
-class CitationSectionForm(ModelForm):
-    class Meta:
-        model = CitationNS
-        fields = ('document', 'content', 'ordering',)
-        widgets = {'document' : forms.widgets.HiddenInput()}
-
-CitationSectionFormset = inlineformset_factory(
-    Note, CitationNS, form=CitationSectionForm, extra=1)
