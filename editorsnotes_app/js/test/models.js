f8274c39fe19a1c5e92b027f67f6448fb9eb0063
Patrick Golden <ptgolden0@gmail.com>
2014-03-14 21:49:34 -0400

    Fix indentation in js tests

--- editorsnotes_app/js/test/models.js
+++ editorsnotes_app/js/test/models.js
@@ -15,23 +15,23 @@ describe('Note', function () {
     });
 
     it('should work fine when passed no attrs with a project', function () {
-       var note = new Note({}, { project: dummyProject });
-       assert.equal(note.project, dummyProject);
+      var note = new Note({}, { project: dummyProject });
+      assert.equal(note.project, dummyProject);
     });
 
     it('should have an absolute url', function () {
-       var note = new Note({}, { project: dummyProject });
-       assert.equal(note.url(), '/api/projects/emma/notes/');
+      var note = new Note({}, { project: dummyProject });
+      assert.equal(note.url(), '/api/projects/emma/notes/');
     });
 
     it('should have an absolute url with ID', function () {
-       var note = new Note({id: 12}, { project: dummyProject });
-       assert.equal(note.url(), '/api/projects/emma/notes/12/');
+      var note = new Note({id: 12}, { project: dummyProject });
+      assert.equal(note.url(), '/api/projects/emma/notes/12/');
     });
 
     it('should have a default status of open', function () {
-       var note = new Note({}, { project: dummyProject });
-       assert.equal(note.get('status'), 'open');
+      var note = new Note({}, { project: dummyProject });
+      assert.equal(note.get('status'), 'open');
     });
 
   });
