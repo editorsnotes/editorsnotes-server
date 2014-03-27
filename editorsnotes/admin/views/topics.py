3d98d8b34b5ecde1133bde866aa4c75ba63a631f
Patrick Golden <ptgolden0@gmail.com>
2014-03-27 12:30:32 -0400

    Change admin views to simply bootstrap data for Backbone

--- editorsnotes/admin/views/topics.py
+++ editorsnotes/admin/views/topics.py
@@ -4,11 +4,33 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 
 from editorsnotes.main.models import Topic, TopicNode
+from editorsnotes.api.serializers import TopicSerializer
 
 from .. import forms
-from common import BaseAdminView
+from common import BaseAdminView, BootstrappedBackboneView
 
-class TopicAdminView(BaseAdminView):
+class TopicAdminView(BootstrappedBackboneView):
+    model = Topic
+    serializer_class = TopicSerializer
+    def get_object(self, topic_node_id=None):
+        return topic_node_id and get_object_or_404(
+            Topic, topic_node_id=topic_node_id, project__slug=self.project.slug)
+    def get_breadcrumb(self):
+        breadcrumbs = (
+            (self.project.name, self.project.get_absolute_url()),
+            ('Topics', reverse('all_topics_view',
+                               kwargs={'project_slug': self.project.slug})),
+        )
+        if self.object is None:
+            breadcrumbs += ( ('Add', None),)
+        else:
+            breadcrumbs += (
+                (self.object.as_text(), self.object.get_absolute_url()),
+                ('Edit', None)
+            )
+        return breadcrumbs
+
+class OldTopicAdminView(BaseAdminView):
     model = Topic
     form_class = forms.TopicForm
     formset_classes = (
