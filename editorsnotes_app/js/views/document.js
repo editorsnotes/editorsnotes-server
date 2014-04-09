eb36f2e9f4f49e77edc6b3b310b71a4525191795
Patrick Golden <ptgolden0@gmail.com>
2014-04-09 16:12:34 -0400

    Get editing documents to work

--- editorsnotes_app/js/views/document.js
+++ editorsnotes_app/js/views/document.js
@@ -5,6 +5,10 @@ var Backbone = require('../backbone')
   , ZoteroDataView = require('./edit_zotero')
 
 module.exports = Backbone.View.extend({
+  events: {
+    'click .save-item': 'saveItem'
+  },
+
   initialize: function (options) {
     var that = this;
 
@@ -13,6 +17,10 @@ module.exports = Backbone.View.extend({
     this.zoteroView = new ZoteroDataView({ zoteroData: that.model.get('zotero_data') });
     this.listenTo(this.zoteroView, 'updatedCitation', function (data) {
       that.$('#document-citation').html(data);
+      that.model.set('description', data);
+    });
+    this.listenTo(this.zoteroView, 'updatedZoteroData', function (data) {
+      that.model.set('zotero_data', data);
     });
 
   },
@@ -20,11 +28,28 @@ module.exports = Backbone.View.extend({
   render: function () {
     var that = this
       , template = require('../templates/document.html')
+      , saveRow = require('../templates/save_row.html')()
 
-    this.$el.html(template({ doc: that.model }));
+    this.$el.html( template({ doc: that.model }) + saveRow );
 
     this.topicListView.$el.appendTo( that.$('#document-related-topics') );
     this.zoteroView.$el.appendTo( that.$('#document-zotero-data') );
+  },
+
+  toggleLoaders: function (state) {
+    this.$('.save-item').prop('disabled', state);
+    this.$('.loader').toggle(state);
+  },
+
+  saveItem: function () {
+    var that = this;
+
+    this.toggleLoaders(true);
+    this.model.save()
+      .always(this.toggleLoaders.bind(this, false))
+      .done(function () {
+        window.location.href = window.location.href.replace(/edit\//, '');
+      });
   }
 
 });
