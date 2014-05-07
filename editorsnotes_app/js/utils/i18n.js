781a32ff9fb388fccaa0a604315fe0c0c15f4f9d
Patrick Golden <ptgolden0@gmail.com>
2014-05-07 15:07:24 -0400

    Add (sparse) English locale for `main`

--- editorsnotes_app/js/utils/i18n.js
+++ editorsnotes_app/js/utils/i18n.js
@@ -1,8 +1,6 @@
 "use strict";
 
-var zoteroI18n = requirePo('./../../../editorsnotes/djotero/locale/%s/LC_MESSAGES/django.po');
-
 module.exports = {
-  main: null,
-  zotero: zoteroI18n
+  main: requirePo('./../../../editorsnotes/main/locale/%s/LC_MESSAGES/django.po'),
+  zotero: requirePo('./../../../editorsnotes/djotero/locale/%s/LC_MESSAGES/django.po')
 }
