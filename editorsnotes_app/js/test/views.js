bbe4821ea0887e6e46f24059dca320f088bff5f4
Patrick Golden <ptgolden0@gmail.com>
2014-06-06 11:47:25 -0400

    Use sinon.FakeXMLHttpRequest to test AJAX

--- editorsnotes_app/js/test/views.js
+++ editorsnotes_app/js/test/views.js
@@ -1,10 +1,17 @@
 "use strict";
 
-var assert= require('assert')
+var assert = require('assert')
+  , sinon = require('sinon')
+
+global.sinon = sinon;
+require('sinon-event');
+require('sinon-fakexhr');
 
 describe('Zotero editing view', function () {
   var ZoteroItemView = require('../views/edit_zotero')
+    , requests = []
     , testData
+    , xhr
 
   testData = {
     itemType: 'book',
@@ -13,8 +20,19 @@ describe('Zotero editing view', function () {
     date: '1931'
   }
 
+  before(function () {
+    xhr = sinon.useFakeXMLHttpRequest();
+    xhr.onCreate = function (xhr1) {
+      requests.push(xhr1);
+    }
+  });
+
   it('should produce the same data that it was asked to render', function (done) {
     var view = new ZoteroItemView({ zoteroData: testData });
+
+    requests.pop().respond(
+      200, {"Content-type": "application/json"}, 
+      JSON.stringify([{ creatorType: 'author', localized: 'Author' }]));
 
     view.on('updatedZoteroData', function (data) {
       assert.deepEqual(data, testData);
@@ -24,4 +42,7 @@ describe('Zotero editing view', function () {
     view.$('.zotero-entry').first().trigger('input');
   });
 
+  after(function () {
+    xhr.restore();
+  });
 });
