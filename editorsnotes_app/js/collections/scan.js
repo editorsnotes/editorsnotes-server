e0f342d4a2ed7f99da7db30042c499f4d55452b8
Patrick Golden <ptgolden0@gmail.com>
2014-06-13 15:52:07 -0400

    Make scan collection extend OrderedCollection

--- editorsnotes_app/js/collections/scan.js
+++ editorsnotes_app/js/collections/scan.js
@@ -1,15 +1,17 @@
 "use strict";
 
-var Backbone = require('../backbone')
+var OrderedCollection = require('./ordered_collection')
   , Scan = require('../models/scan')
 
-module.exports = Backbone.Collection.extend({
+module.exports = OrderedCollection.extend({
   model: Scan,
   initialize: function (models, options) {
     if (!options.document) {
       throw "Scan collection must be passed a document in its options."
     }
     this.dokument = options.document;
+
+    OrderedCollection.prototype.initialize.apply(this, arguments);
   },
   url: function () { return this.dokument.url() + 'scans/'; }
 });
