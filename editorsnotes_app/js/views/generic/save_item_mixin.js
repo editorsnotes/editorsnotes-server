3bf8fd4a1e3edc27a51dfcfc0f0a9b4e0e305342
Patrick Golden <ptgolden0@gmail.com>
2014-06-17 14:04:05 -0400

    Add ability to delete items to editing interface

--- editorsnotes_app/js/views/generic/save_item_mixin.js
+++ editorsnotes_app/js/views/generic/save_item_mixin.js
@@ -1,10 +1,14 @@
 "use strict";
 
-var saveRow = require('../../templates/save_row.html')();
+var _ = require('underscore')
+  , $ = require('../../jquery')
+  , saveRow = require('../../templates/save_row.html')();
 
 module.exports = {
   events: {
-    'click .save-item': '_handleSave'
+    'click .save-item': '_handleSave',
+    'click .delete-item': '_handleDelete'
+    
   },
   render: function () {
     this.$el.append(saveRow)
@@ -13,15 +17,15 @@ module.exports = {
     this.$('.save-item').prop('disabled', state);
     this.$('.loader').toggle(state);
   },
+  handleError: function (errorObj) {
+    alert(window.JSON.stringify(errorObj));
+  },
   _handleSave: function () {
     if (this.saveItem) {
       this.saveItem();
     } else {
       this.defaultSave();
     }
-  },
-  handleError: function (errorObj) {
-    alert(window.JSON.stringify(errorObj));
   },
   defaultSave: function () {
     var that = this;
@@ -35,5 +39,32 @@ module.exports = {
       .fail(function (jqXHR, textStatus, error) {
         that.handleError(jqXHR.responseJSON);
       });
+  },
+  _handleDelete: function () {
+    if (this.deleteItem) {
+      this.deleteItem();
+    } else {
+      this.defaultDelete();
+    }
+  },
+  defaultDelete: function () {
+    var that = this
+      , template = require('../../templates/confirm_delete.html')
+      , promise = $.get(this.model.url() + 'confirm_delete/')
+
+    promise.done(function (data) {
+      var modal = template({ _ : _, items: data.items })
+        , $modal = $(modal).appendTo('body').modal()
+
+      $modal
+        .on('hidden', $modal.remove)
+        .find('.btn-delete-item').on('click', function () {
+          that.model.destroy()
+            .done(function () {
+              window.location.href = that.model.project.url().replace('\/api\/', '/');
+            })
+            .fail(function (jqXHR) { that.handleError(jqXHR.responseJSON) });
+        });
+    });
   }
 }
