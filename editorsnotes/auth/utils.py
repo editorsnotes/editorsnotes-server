13c771cfc8c7a40c92b737860ee1c341f4fb70a4
Patrick Golden <ptgolden0@gmail.com>
2015-09-27 17:46:04 -0400

    Fix a bug where protocol was doubled when using SITE_URL

--- editorsnotes/auth/utils.py
+++ editorsnotes/auth/utils.py
@@ -9,22 +9,17 @@ def send_activation_email(request, user):
     token_generator = PasswordResetTokenGenerator()
     token = token_generator.make_token(user)
 
-    site_url = '{protocol}://{site}'.format(
-        protocol='https' if request.is_secure() else 'http',
-        site=settings.SITE_URL
-    )
-
     if user.is_active:
         raise Exception('Will not send activation key to active user')
 
     send_mail(
         'Activate your Editors\' Notes account',
 
-        'This email was used to create an account at {site_url}.\n\n'
+        'This email was used to create an account at {settings.SITE_URL}.\n\n'
         'To activate your account, visit the following link:\n\n'
-        '\t{site_url}{activation_url}\n\n'
+        '\t{settings.SITE_URL}{activation_url}\n\n'
         'If you did not request an account, please ignore this email.'.format(
-            site_url=site_url,
+            site_url=settings.SITE_URL,
             activation_url=reverse('auth:activate_account', args=[b64uid, token]),
             activation_token=token),
 
