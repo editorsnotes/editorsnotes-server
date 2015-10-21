fb5245c5b6218560f3b3e78fa1b2667d5430ca2e
Patrick Golden <ptgolden0@gmail.com>
2015-10-21 15:21:12 -0400

    Refactor hydra serializers a bit

--- editorsnotes/api/serializers/hydra.py
+++ editorsnotes/api/serializers/hydra.py
@@ -25,10 +25,95 @@ def url_pattern_for_name(patterns, view_name):
     return found
 
 
+def view_permissions(view_obj, method):
+    request = namedtuple('FakeRequest', 'method')(method)
+
+    return [
+        permission.get_view_permissions(request, view_obj)
+        for permission in view_obj.get_permissions()
+    ]
+
 SUPPORTED_HYDRA_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
 
 
-class HyperlinkedHydraPropertySerializer(serializers.Serializer):
+class ReplaceLDFields(object):
+    def __init__(self, *args, **kwargs):
+        super(ReplaceLDFields, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field.startswith('hydra_'):
+                hydra_name = field.replace('hydra_', 'hydra:')
+                self.fields[hydra_name] = self.fields.pop(field)
+            if field.startswith('jsonld_'):
+                jsonld_name = field.replace('jsonld_', '@')
+                self.fields[jsonld_name] = self.fields.pop(field)
+
+
+class HydraClassSerializer(ReplaceLDFields, serializers.Serializer):
+    "Serializer to generate a hydra:Class based on a serializer."
+    jsonld_id = serializers.SerializerMethodField()
+    jsonld_type = serializers.SerializerMethodField()
+
+    label = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
+
+    supportedProperty = serializers.SerializerMethodField()
+
+    def get_jsonld_id(self, obj):
+        project = self.root.project  # ?
+        return '{}:{}'.format(project.slug, self.get_label())
+
+    def get_jsonld_type(self, obj):
+        return 'hydra:Class'
+
+    def get_label(self, obj):
+        words = self.field_name.split('_')
+        return ''.join(map(str.title, words))
+
+    def get_description(self, obj):
+        return
+
+    def get_supported_property(self):
+        return [
+            HydraPropertySerializer(field, property_name, context=self.context)
+            for property_name, field in self.get_fields().items()
+        ]
+
+
+class HydraPropertySerializer(ReplaceLDFields, serializers.Serializer):
+    """
+    Serializer to generate a hydra:supportedProperty based on a serializer
+    field.
+    """
+    property = serializers.SerializerMethodField()
+    hydra_title = serializers.SerializerMethodField()
+    hydra_description = serializers.ReadOnlyField(source='help_text')
+    hydra_required = serializers.ReadOnlyField(source='required')
+    hydra_writeonly = serializers.ReadOnlyField(source='write_only')
+    hydra_readonly = serializers.ReadOnlyField(source='read_only')
+
+    def __init__(self, instance, property_name, domain=None, **kwargs):
+        self.domain = domain
+        self.property_name = property_name
+        self.context_dict = kwargs.pop('context_dict', CONTEXT)
+        super(HydraPropertySerializer, self).__init__(instance, **kwargs)
+
+    def get_property(self, obj):
+        if isinstance(obj, serializers.HyperlinkedRelatedField):
+            return HyperlinkedHydraPropertySerializer(
+                obj, self.property_name, self.domain
+            ).data
+        return self.context_dict[self.property_name]
+
+    def get_hydra_title(self, obj):
+        return self.property_name
+
+
+class HyperlinkedHydraPropertySerializer(ReplaceLDFields,
+                                         serializers.Serializer):
+    """
+    Serializer to generate a hydra:property based on a field that is an
+    instance of DRF's HyperlinkedRelatedField.
+    """
     jsonld_id = serializers.SerializerMethodField()
     jsonld_type = serializers.SerializerMethodField()
 
@@ -43,19 +128,9 @@ class HyperlinkedHydraPropertySerializer(serializers.Serializer):
     def __init__(self, instance, property_name, domain=None, **kwargs):
         self.domain = domain
         self.property_name = property_name
-
         self.view_class = self._get_view_class(instance)
-
         super(HyperlinkedHydraPropertySerializer, self)\
             .__init__(instance, **kwargs)
-
-        for field in self.fields:
-            if field.startswith('hydra_'):
-                hydra_name = field.replace('hydra_', 'hydra:')
-                self.fields[hydra_name] = self.fields.pop(field)
-            if field.startswith('jsonld_'):
-                jsonld_name = field.replace('jsonld_', '@')
-                self.fields[jsonld_name] = self.fields.pop(field)
 
     def _get_view_class(self, obj):
         view_name = obj.view_name
@@ -76,6 +151,18 @@ class HyperlinkedHydraPropertySerializer(serializers.Serializer):
 
         return pattern.callback.cls
 
+    def _get_retrieve_operation(self, obj):
+        return {
+            '@id': '_:project_notes_retrieve',
+            '@type': 'hydra:Operation',
+            'hydra:method': 'GET',
+            'label': 'Retrieve all notes for this project.',
+            'description': None,
+            'expects': None,
+            'returns': self.get_range(obj),
+            'statusCodes': []
+        }
+
     def _is_collection_view(self):
         return issubclass(self.view_class, mixins.ListModelMixin)
 
@@ -94,21 +181,8 @@ class HyperlinkedHydraPropertySerializer(serializers.Serializer):
             (self.domain + '/' + self.get_label(obj).title())
         )
 
-
     def get_domain(self, obj):
         return self.domain
-
-    def _get_retrieve_operation(self, obj):
-        return {
-            '@id': '_:project_notes_retrieve',
-            '@type': 'hydra:Operation',
-            'hydra:method': 'GET',
-            'label': 'Retrieve all notes for this project.',
-            'description': None,
-            'expects': None,
-            'returns': self.get_range(obj),
-            'statusCodes': []
-        }
 
     def get_hydra_supportedOperation(self, obj):
         operations = []
@@ -122,80 +196,13 @@ class HyperlinkedHydraPropertySerializer(serializers.Serializer):
                 operations.append(self._get_retrieve_operation(obj))
                 continue
 
-            required_permissions, = [
-                permission.get_view_permissions(
-                    namedtuple('FakeRequest', 'method')(method),
-                    view_obj)
-                for permission in view_obj.get_permissions()
-            ]
+            required_permissions = view_permissions(view_obj, method)
 
         return operations
 
 
-
-class HydraPropertySerializer(serializers.Serializer):
-    property = serializers.SerializerMethodField()
-    hydra_title = serializers.SerializerMethodField()
-    hydra_description = serializers.ReadOnlyField(source='help_text')
-    hydra_required = serializers.ReadOnlyField(source='required')
-    hydra_writeonly = serializers.ReadOnlyField(source='write_only')
-    hydra_readonly = serializers.ReadOnlyField(source='read_only')
-
-    def __init__(self, instance, property_name, domain=None, **kwargs):
-        self.domain = domain
-        self.property_name = property_name
-        self.context_dict = kwargs.pop('context_dict', CONTEXT)
-
-        super(HydraPropertySerializer, self).__init__(instance, **kwargs)
-
-        for field in self.fields:
-            if field.startswith('hydra_'):
-                hydra_name = field.replace('hydra_', 'hydra:')
-                self.fields[hydra_name] = self.fields.pop(field)
-
-    def get_property(self, obj):
-        if isinstance(obj, serializers.HyperlinkedRelatedField):
-            return HyperlinkedHydraPropertySerializer(
-                obj, self.property_name, self.domain
-            ).data
-        return self.context_dict[self.property_name]
-
-    def get_hydra_title(self, obj):
-        return self.property_name
-
-
-class HydraClassSerializer(serializers.Serializer):
-    ld_id = serializers.SerializerMethodField()
-    ld_type = serializers.SerializerMethodField()
-
-    label = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
-
-    supportedProperty = serializers.SerializerMethodField()
-
-    def get_ld_id(self, obj):
-        project = self.root.project  # ?
-        return '{}:{}'.format(project.slug, self.get_label())
-
-    def get_ld_type(self, obj):
-        return 'hydra:Class'
-
-    def get_label(self, obj):
-        words = self.field_name.split('_')
-        return ''.join(map(str.title, words))
-
-    def get_description(self, obj):
-        return
-
-    def get_supported_property(self):
-        return [
-            HydraPropertySerializer(field, property_name, context=self.context)
-            for property_name, field in self.get_fields().items()
-        ]
-
-
-# common_hyperlinks = ...
 class ProjectHydraClassesSerializer(serializers.Serializer):
+    "Serializer to generate Hydra documentation for a project."
     project = HydraClassSerializer(en_serializers.ProjectSerializer)
 
     user = HydraClassSerializer(en_serializers.NoteSerializer)
