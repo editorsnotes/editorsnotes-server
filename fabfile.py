c0d75e9f562c9a2db45a461a3ba183c2b780805b
Patrick Golden <ptgolden0@gmail.com>
2016-05-17 11:40:24 -0400

    Use make/django-admin.py instead of fabric
    Not everything is ported over yet, but this is enough to get travis
    rolling.

--- fabfile.py
+++ fabfile.py
@@ -1,45 +1,4 @@
-from fabric.api import env, local, lcd
-from fabric.colors import red, green
-from fabric.decorators import task, runs_once
-from fabric.operations import prompt
-from fabric.utils import abort
-
-import datetime
-import fileinput
-import importlib
-import os
-import random
-import re
-import subprocess
-import sys
-import time
-
-PROJ_ROOT = os.path.dirname(env.real_fabfile)
-env.project_name = 'editorsnotes'
-env.python = 'python' if 'VIRTUAL_ENV' in os.environ else './bin/python'
-
-@task
-def setup():
-    """
-    Set up a local development environment
-
-    This command must be run with Fabric installed globally (not inside a
-    virtual environment)
-    """
-    if os.getenv('VIRTUAL_ENV') or hasattr(sys, 'real_prefix'):
-        abort(red('Deactivate any virtual environments before continuing.'))
-    make_settings()
-    make_virtual_env()
-    collect_static()
-    print ('\nDevelopment environment successfully created.\n' +
-           'Create a Postgres database, enter its information into ' +
-           'editorsnotes/settings_local.py, and run `fab sync_database` to finish.')
-
-@task
-def test():
-    "Run the test suite locally."
-    with lcd(PROJ_ROOT):
-        local('{python} manage.py test'.format(**env))
+raise EnvironmentError("Fabric is no longer used to issue management commands")
 
 @task
 def sync_database():
@@ -66,50 +25,6 @@ def sync_database():
                   '"{name}" {slug} --users={username}'.format(
                       name=project_name, slug=project_slug,
                       username=username, **env))
-
-@task
-def runserver():
-    "Run the development server"
-    with lcd(PROJ_ROOT):
-        local('{python} manage.py runserver'.format(**env))
-
-@task
-def generate_blank_settings():
-    settings_path = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)),
-        'editorsnotes/example-settings_local.py')
-    secret_key = generate_secret_key()
-
-    with open(settings_path) as settings_file:
-        settings_str = settings_file.read()
-
-    settings_str = settings_str.replace(
-        "SECRET_KEY = ''", "SECRET_KEY = '{}'".format(secret_key))
-
-    print settings_str
-    return settings_str
-
-@task
-@runs_once
-def make_settings():
-    """
-    Generate a local settings file.
-
-    Without any arguments, this file will go in editorsnotes/settings_local.py.
-    If the function is passed an argument that defines env.hosts, this file will
-    be placed in the deploy directory with the name settings-{host}.py
-    """
-    to_create = (['deploy/settings-{}.py'.format(host) for host in env.hosts]
-                 or ['editorsnotes/settings_local.py'])
-
-    for settings_file in to_create:
-        secret_key = generate_secret_key()
-        with lcd(PROJ_ROOT):
-            local('if [ ! -f {0} ]; then cp {1} {0}; fi'.format(
-                settings_file, 'editorsnotes/example-settings_local.py'))
-            for line in fileinput.input(settings_file, inplace=True):
-                print line.replace("SECRET_KEY = ''",
-                                   "SECRET_KEY = '{}'".format(secret_key)),
 
 @task
 def create_cache_tables():
@@ -154,5 +69,5 @@ def collect_static():
         local('{python} manage.py collectstatic --noinput -v0'.format(**env))
 
 def generate_secret_key():
-    SECRET_CHARS = 'abcdefghijklmnopqrstuvwxyz1234567890-=!@#$$%^&&*()_+'
+    SECRET_CHARS = 'abcdefghijklmnopqrstuvwxyz1234567890-=!@#$%^&*()_+'
     return ''.join([random.choice(SECRET_CHARS) for i in range(50)])
